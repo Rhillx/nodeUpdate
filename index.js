bd568bea1b98d4782fab3edf918aa7ea61227a61
Richard Hill III <rhillz718@gmail.com>
2017-03-11 13:53:30 -0500

    changed method from osmosis to cheerio..Still need to do comparson

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-const osmosis = require ('osmosis')
+const request = require('request')
+const cheerio = require('cheerio')
 const url = 'https://nodejs.org/en/'
-const anchor = '//a[@class="home-downloadbutton"]'
+
 const exec = require('child_process').exec;
-const cmd = 'node -v'
-
-osmosis
-.get(url)
-.find(anchor)
-.follow('@href')
-.log(console.log)
-.error(console.log)
-.debug(console.log)
-
-.data((listing)=>{
-  exec(cmd, (err, stdout)=>{
-    if (err){
-      console.log(err);
-    }
-    else{
-      console.log(stdout);
-    }
-  })
+const cmd = 'node -v';
 
 
+request(url, (err, res, body)=>{
+  const $ = cheerio.load(body);
+  const currentNodeVersion = $('.home-downloadbutton')
+  const currentNodeVersionText =$('.home-downloadbutton').text();
+
+  console.log(currentNodeVersionText);
+
+})
+
+exec(cmd,(error, stdout, stderr) => {
+  if(error){
+    console.log(error);
+  }
+  else{
+    console.log(stdout);
+  }
 });
